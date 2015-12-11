535035f0ba2503841e67f438f37431739752ef5d
Mai Huy <mhuy1602@gmail.com>
2015-12-12 00:39:35 +0700

    Classify by Naive Bayes

--- naive_bayes.py
+++ naive_bayes.py
@@ -22,7 +22,31 @@ def train(X, y):
 	'''
 	
 	# TODO 
-	return (0, np.zeros(X.shape[0]), np.zeros(X.shape[0]))
+	# Cal phi
+	phi = 0.0
+	nXRow, nXCol = X.shape
+	vecLabelMail = y[:, 0]
+	arrSpam = 0
+	arrSpam = [1 for x in vecLabelMail if x == 1]
+	phi = len(arrSpam)*1.0/len(vecLabelMail)
+
+	# Cal phi0 and phi1
+	phi0 = []
+	phi1 = []
+	for vecMaili in X.T:
+		arrNumeratorPhi0 = [1]
+		arrNumeratorPhi1 = [1]
+		
+		arrNumeratorPhi0 = arrNumeratorPhi0 + [1 for m, l in zip(vecMaili, vecLabelMail) if m == 1 and l == 0]
+		arrNumeratorPhi1 = arrNumeratorPhi1 + [1 for m, l in zip(vecMaili, vecLabelMail) if m == 1 and l == 1]
+
+		denominatorPhi0 = 2 + (1 - phi)
+		denominatorPhi1 = 2 + (phi)
+
+		phi0.append((len(arrNumeratorPhi0)/denominatorPhi0))
+		phi1.append((len(arrNumeratorPhi1)/denominatorPhi1))
+
+	return (phi, phi0, phi1)
 
 def predict_spam(phi, phi0, phi1, X_test):
 	'''
